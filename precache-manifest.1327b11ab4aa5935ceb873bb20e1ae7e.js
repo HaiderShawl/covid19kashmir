28357f9226194937e32675c3f8e43e5f4244a599
HaiderShawl <34603371+HaiderShawl@users.noreply.github.com>
2021-06-18 14:08:42 +0530

    Updates

---
+++ precache-manifest.1327b11ab4aa5935ceb873bb20e1ae7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40451eddea1967661d7ef0ff50b1f478",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71990babcd2e354827c9",
+    "url": "/static/css/main.717c4f12.chunk.css"
+  },
+  {
+    "revision": "7e6e50382b7fbc6767b0",
+    "url": "/static/js/2.c4cc149b.chunk.js"
+  },
+  {
+    "revision": "774152ec3dfb5dadc6880f2e7f79e400",
+    "url": "/static/js/2.c4cc149b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71990babcd2e354827c9",
+    "url": "/static/js/main.b2dbceb5.chunk.js"
+  },
+  {
+    "revision": "20e74bb56b5aad9a2c37",
+    "url": "/static/js/runtime-main.96a74c67.js"
+  }
+]);
